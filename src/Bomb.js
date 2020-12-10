ffadb79b4fc7c70fe9b74f4a6d7fa241c325abe1
JakesProjects1 <jacob@kagon.net>
2020-12-09 19:13:59 -0500

    done

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,36 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+
+
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    increment = () => {
+       const newCount = this.state.secondsLeft - 1
+       this.setState({
+           secondsLeft: newCount
+       })
+    }
+    render() {
+        if(this.state.secondsLeft === 0) {
+    
+        return (
+            <div>
+            Boom! 
+            </div>
+        );
+    } else {
+        return(
+            <div>{this.state.secondsLeft} seconds left before I go boom!</div>
+        )
+    }
+    }
+}
+
+export default Bomb;
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,40 @@
 // your ImageSlider code here!
+
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    increment = () => {
+        const newSlider = this.state.currentSlideIndex + 1
+        this.setState({
+            currentSlideIndex: newSlider
+        })
+    }
+
+    
+
+    
+
+
+    render() {
+        const CURRENT_SLIDE = this.state.currentSlideIndex
+
+        if (CURRENT_SLIDE === this.state.currentSlideIndex) {
+        return (
+            <div>
+            I am on slide {CURRENT_SLIDE}
+            </div>
+        );
+        }
+    }
+}
+
+export default ImageSlider;
+
